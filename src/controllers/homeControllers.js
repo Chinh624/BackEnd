2e685b866cf655cca8cd9f26e6d6ecc119194c04
D.Chinh <chinh6803@gmail.com>
2023-12-27 00:50:59 +0700

    connect-sql-project

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -1,14 +1,15 @@
-const getHomePage = (req,res) =>{
-    res.send("Hello World!");
-}
+const getHomePage = (req, res) => {
+  res.send("Hello World!");
+};
 
-const getExample = (req,res) =>{
-    res.render("index.ejs");
-}
+const getExample = (req, res) => {
+  res.render("index.ejs");
+};
 
 module.exports = {
-    getHomePage,
-    getExample
-}
+  getHomePage,
+  getExample,
+};
 
 // export many variable
+    
--- src/server.js
+++ src/server.js
@@ -3,6 +3,30 @@ const app = express();// app express
 const port = 8080; // port => hardcode
 const viewEngine = require("./config/viewEngine"); // import config 
 const webRouter = require("./routers/web"); // router
+// get the client
+const mysql = require('mysql2');
+// create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3307, 
+  // if not null default 3306
+  user: 'root', // empty
+  password:'123456',
+  database: 'DataLearn',
+});
+
+connection.query(
+  'SELECT * FROM Users u',
+  function(err, results, fields) {
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+  }
+);
+
+
+
+
+
 
 viewEngine(app)
 app.use(webRouter);
